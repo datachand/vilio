68f448d883a01d39e5af6e59013e88164997f83e
Cody Lay <data.chand@gmail.com>
2013-12-02 00:44:33 +0530

    Just Started

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Vilio::Application.config.secret_key_base = 'fd107dd4342d654ed30c7fb69077d0ae8fb6bf4c93c2be0ed005c1fa7dc44b657240ecce5714415a85a9ef95ae7440507bdc2b597e2c1a41a98c837905f34ceb'
